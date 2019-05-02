76742ae032c94dd8a5338b3e073c134745e00d9e
eteries <internet.practica@gmail.com>
2019-05-03 00:18:34 +0300

    add propTypes and first prop (#7)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app/app.jsx';
 
+const cardTitles = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`
+];
+
 ReactDOM.render(
-    <App />,
+    <App titles={cardTitles} />,
     document.getElementById(`root`)
 );
