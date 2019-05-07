8d8ef56ebb48baa2fcc8638199aad3041d4fe6d6
eteries <internet.practica@gmail.com>
2019-05-08 02:24:32 +0300

    add mock offers based on design and spec (#11.2)

---
+++ src/mocks/offers.js
@@ -0,0 +1,34 @@
+const offers = [
+  {
+    url: `apartment-01.jpg`,
+    isPremium: true,
+    price: 120,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `apartment`,
+    rating: 93
+  },
+  {
+    url: `img/room.jpg`,
+    isPremium: false,
+    price: 80,
+    name: `Wood and stone place`,
+    type: `private room`,
+    rating: 72
+  },
+  {
+    url: `img/apartment-02.jpg`,
+    isPremium: false,
+    price: 132,
+    name: `Canal View Prinsengracht`,
+    type: `house`,
+    rating: 85
+  },
+  {
+    url: `img/apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `hotel`,
+    rating: 80
+  },
+];
