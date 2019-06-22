1af767252dae124293bca1b14a847adc7bc39b72
Сергей Яньшин <yanshin.s@spb.helix.ru>
2019-06-22 19:41:27 +0300

    add devtools (#18.1)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import {createStore} from 'redux';
 import {reducer} from './reducer';
 import {Provider} from 'react-redux';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+    typeof window !== undefined &&
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
